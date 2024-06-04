69cc045dec17e722fb0a7a7411947bf609dd9008
DAvalos36 <davalos@itparral.edu.mx>
2024-06-04 16:27:10 -0600

    feat: util to manage pk prefix

--- utils.ts
+++ utils.ts
@@ -17,3 +17,27 @@ export function quitPrefix({
 
 	return id;
 }
+
+function easyQuitPrefix(pk: string): string {
+	if (pk.includes("#")) {
+		pk.split("#")[1];
+	}
+	return pk;
+}
+
+export function arrayQuitPrefix<T>({
+	arr,
+	indexes,
+}: { arr: T[]; indexes: (keyof T)[] }): T[] {
+	return arr.map((item) => {
+		const newItem = { ...item };
+		indexes.forEach((index) => {
+			if (newItem[index] && typeof newItem[index] === "string") {
+				newItem[index] = easyQuitPrefix(
+					newItem[index] as unknown as string,
+				) as unknown as T[keyof T];
+			}
+		});
+		return newItem;
+	});
+}
