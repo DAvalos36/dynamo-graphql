74587494d96bf3e2c6e2ffaa9dfddbc9211aef4f
DAvalos36 <davalos@itparral.edu.mx>
2024-06-07 11:39:16 -0600

    wip: auth

--- index.ts
+++ index.ts
@@ -7,10 +7,15 @@ import { deleteContainer, deleteTodo } from "./resolvers/deletes";
 
 import { readFileSync } from "node:fs";
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import { graphql, GraphQLError } from "graphql";
+import { expressMiddleware } from "@apollo/server/express4";
 import { jwtVerify } from "jose";
 import { env } from "node:process";
+import cors from "cors";
+
+import express from "express";
+import { GraphQLError } from "graphql";
+
+const app = express();
 
 const typeDefs = readFileSync("./graphql/schema.graphql", {
 	encoding: "utf-8",
@@ -112,19 +117,35 @@ const server = new ApolloServer({
 	resolvers,
 });
 async function main() {
-	const { url } = await startStandaloneServer(server, {
-		context: async ({ req }) => {
-			const token = req.headers.authorization || "";
+	await server.start();
 
-			try {
-				const e = await jwtVerify(token, textEncoder.encode(env.JWT_SECRET));
-				console.log("JWT TODO BIEN", { e });
-				return { user: e.payload };
-			} catch (error) {
-				return { user: undefined };
-			}
-		},
-	});
+	app.use(
+		"/graphql",
+		cors<cors.CorsRequest>(),
+		express.json(),
+		expressMiddleware(server, {
+			context: async ({ req }) => {
+				try {
+					let token = req.headers.authorization || "";
+					token = token.split(" ")[1];
+					const e = await jwtVerify(token, textEncoder.encode(env.JWT_SECRET));
+					console.log(e.payload);
+					return { user: e.payload };
+				} catch (error) {
+					throw new GraphQLError("User is not authenticated", {
+						extensions: {
+							code: "UNAUTHENTICATED",
+							http: { status: 401 },
+						},
+					});
+				}
+			},
+		}),
+	);
 }
 
 main();
+
+app.listen(3000, () => {
+	console.log("Corriendo");
+});
